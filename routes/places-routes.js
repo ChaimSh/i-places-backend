5f64739cae1e65e121f76808ae473f621f129a4c
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-10-01 22:13:03 -0500

    api places

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
+
+
+
 router.get('/', (req, res, next) => {
     console.log('GET Request in PLaces');
     res.json({message: 'It works!'});
