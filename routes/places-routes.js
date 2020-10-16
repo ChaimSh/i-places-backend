4ad76d9ec81defe9e4729914fca247c54eada9ba
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-10-15 21:52:25 -0500

    move routing logic over to controller to free up routing file

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,48 +1,9 @@
 const express = require('express');
 
-const HttpError = require('../models/http-error');
-
 const router = express.Router();
 
-const DUMMY_PLACES = [{
-    id: 'p1',
-    title: 'Empire State building',
-    description: 'very tall building',
-    location: {
-        lat: 40.7484474,
-        lng: -73.9871516
-    },
-    address: '20 W #4th St, New York, NY 10001',
-    creator: 'u1'
-}]
+router.get('/:pid',);
 
-
-router.get('/:pid', (req, res, next) => {
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p => {
-        return p.id === placeId;
-    })
-
-    if (!place) {
-       throw new HttpError('Could not find a place for provded id', 404);
-    }
-
-    res.json({place});
-});
-
-router.get('/user/:uid', (req, res, next) => {
-    const userId = req.params.uid;
-    const user = DUMMY_PLACES.find(p => {
-        return p.creator === userId;
-    });
-
-    if (!place) {
-       return next(
-           new HttpError('Could not find a place for provded id', 404)
-           );
-     }
-
-    res.json({place});
-});
+router.get('/user/:uid', );
 
 module.exports = router;
