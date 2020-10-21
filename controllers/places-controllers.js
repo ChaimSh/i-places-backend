75eb21d22a65aebefe2aa87e16580b7a78d436dd
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-10-21 12:38:30 -0500

    set up update place in place controller to find place by index and update the place with an updated COPY of the origionally found place

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -59,6 +59,16 @@ const createPlace = (req, res, next) => {
     res.status(201).json({place: createdPlace});
 };
 
+const updatePlace = (req, res, next) => {
+    const {title, description} = req.body;
+};
+
+const deletePlace = (req, res, next) => {
+
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
