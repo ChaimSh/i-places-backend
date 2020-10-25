5ad53262320de9bdc9b5ca8b686b9e9bb491ef8b
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-10-25 16:40:01 -0500

    error fix

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -90,7 +90,7 @@ const deletePlace = (req, res, next) => {
     const placeId = req.params.pid;
     if(!DUMMY_PLACES.find(p => p.id === placeId)) {
         throw new HttpError('Could not find a plce with that id', 404);
-    }
+    }  
   
     DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
    res,status(200).json({massage: 'Deleted place.'});
