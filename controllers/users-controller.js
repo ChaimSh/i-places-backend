759f1c35ce4cccf3e507ab187553829d2707fbd0
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-10-28 20:45:20 -0500

    set up users controller

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,8 +1,44 @@
-const getUsers = (req, res, next) => {};
+const uuid = require('uuid/v4');
+const HttpError = require('../models/http-error');
 
-const signup = (req, res, next) => {};
 
-const login = (req, res, next) => {};
+const DUMMY_USERS = [
+    {
+        id: 'u1',
+        name: 'Chaim S.',
+        email: 'test@test.com',
+        password: 'testers'
+    }
+];
+
+const getUsers = (req, res, next) => {
+    res.json({users:DUMMY_USERS});
+};
+
+const signup = (req, res, next) => {
+    const {name, email, password} = req.body
+    
+    const createdUser = {
+        id: uuid(),
+        name,
+        email,
+        password
+      };
+     
+      DUMMY_USERS.push(createdUser);
+     
+      res.status(201).json({user: createdUser});
+};
+
+const login = (req, res, next) => {
+  const {email, password} = req.body;
+
+  const identifiedUser = DUMMY_USERS.find(u => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+     throw new HttpError('Could not identify user. Check credentials.', 401);
+  } 
+  res.json({message: 'Logged in!'});
+};
 
 exports.getUsers = getUsers;
 exports.signup = signup;
