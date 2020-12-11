0c90de10ec9d011d24dacaf9849abc2d6732eeba
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-12-10 21:52:44 -0600

    install mongoose

--- util/location.js
+++ util/location.js
@@ -1,10 +1,9 @@
 const axios = require('axios');
 const HttpError = require('../models/http-error');
 
-const API_KEY = 'AIzaSyCdHoftFE53PWMUrY2t_MbD7OY-lE3C3Xg';
 
 async function getCoordsForAddress(address) {
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`);
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}`);
 
     const data = response.data
 
