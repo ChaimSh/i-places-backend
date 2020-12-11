0a81b12a900ae93d7b46a54ac0552d5a314d25d5
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-12-10 21:54:09 -0600

    install mongoose

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   const error = new HttpError('Could not find this route', 404);
   throw error;
 });
-
+ 
 app.use((error, req, res, next) => {
     if (res.headerSent) {
       return next(error)
