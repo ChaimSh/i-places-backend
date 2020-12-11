2af95fca0a385b2dc118b19856e48ecdc92b12a0
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-12-10 21:58:39 -0600

    use server only if mongoose workrs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require('./routes/users-routes');
@@ -29,4 +30,9 @@ app.use((error, req, res, next) => {
     res.json({message: error.message || "an unknown error occurred"});
 });
 
-app.listen(5000);
+mongoose
+.connect()
+.then(() => {
+  app.listen(5000);
+})
+.catch()
