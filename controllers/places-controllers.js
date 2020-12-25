7637710be2c4bf0299315c8ffdb93b4d883c5bc3
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-12-24 18:26:30 -0600

    apply try catch method to the above

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -40,10 +40,18 @@ const getPlaceById = async (req, res, next) => {
     res.json({place: place.toObject({getters: true}) });
 };
 
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
     const userId = req.params.uid;
-    const places = Place.find({ creator: userId });
-
+    
+    let places;
+    try {
+         places = await Place.find({ creator: userId });
+    } catch(err) {
+        const error = new HttpError(
+            'Fetching placed failed, please try again later.'
+        )
+    }
+    
     if (!places || places.length === 0) {
        return next(
            new HttpError('Could not find places for provded id', 404)
